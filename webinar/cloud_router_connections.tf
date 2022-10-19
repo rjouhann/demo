422cde053c91962c6592964760ba3900444f5592
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-10-19 11:41:37 -0700

    Comment out packetfabric_cloud_router_bgp_session.crbs_2

--- webinar/cloud_router_connections.tf
+++ webinar/cloud_router_connections.tf
@@ -4,7 +4,7 @@ data "packetfabric_cloud_router_connections" "all_crc" {
 
   depends_on = [
     packetfabric_cloud_router_bgp_session.crbs_1,
-    packetfabric_cloud_router_bgp_session.crbs_2
+    # packetfabric_cloud_router_bgp_session.crbs_2
   ]
 }
 output "packetfabric_cloud_router_connections" {
