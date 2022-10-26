1d217ac8a5037eb925c92e26ee03b0b35874fc9b
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-10-26 11:15:12 -0700

    cleanup

--- webinar/cloud_router_connections.tf
+++
@@ -1,7 +0,0 @@
-data "packetfabric_cloud_router_connections" "all_crc" {
-  provider   = packetfabric
-  circuit_id = packetfabric_cloud_router.cr.id
-}
-output "packetfabric_cloud_router_connections" {
-  value = data.packetfabric_cloud_router_connections.all_crc
-}
